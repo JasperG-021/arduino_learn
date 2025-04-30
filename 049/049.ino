9bd8d6f4b3a0c92cedc9e159ca4f9748dc97ba9e
JasperG021 <kakuyasukuni@gmail.com>
2025-04-30 14:20:58 +0900

    update 049

--- 049/049.ino
+++ 049/049.ino
@@ -1,37 +1,35 @@
 #include <Stepper.h>
 #define STEP_AROUND 2038.
 #define RPM 15
+#define STEP_ONE_DEGREE STEP_AROUND / 360.
 Stepper stepper_motor_test(STEP_AROUND, 2, 3, 4, 5);
 
-int pin_10 = A4;
-int pin_1 = A5;
-int pin_010 = A0;
-int pin_01 = A1;
+#define PIN_F90 A4
+#define PIN_F10 A5
+#define PIN_B90 A0
+#define PIN_B10 A1
 
-int val_button_10_old;
-int val_button_010_old;
-
-float STEP_ONE_DEGREE = STEP_AROUND / 360.;
+int val_button_f90_old;
+int val_button_b90_old;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(pin_10, INPUT_PULLUP);
-  pinMode(pin_1, INPUT_PULLUP);
-  pinMode(pin_010, INPUT_PULLUP);
-  pinMode(pin_01, INPUT_PULLUP);
+  pinMode(PIN_F90, INPUT_PULLUP);
+  pinMode(PIN_F10, INPUT_PULLUP);
+  pinMode(PIN_B90, INPUT_PULLUP);
+  pinMode(PIN_B10, INPUT_PULLUP);
 }
 
 void loop() {
-  int val_empt = 9;
-  int val_button_10 = digitalRead(pin_10);
-  int val_button_1 = digitalRead(pin_1);
-  int val_button_010 = digitalRead(pin_010);
-  int val_button_01 = digitalRead(pin_01);
+  int val_button_f90 = digitalRead(PIN_F90);
+  int val_button_f10 = digitalRead(PIN_F10);
+  int val_button_b90 = digitalRead(PIN_B90);
+  int val_button_b10 = digitalRead(PIN_B10);
 
-  button_control(val_button_10, STEP_ONE_DEGREE*10, val_button_10_old);
-  button_control(val_button_1, STEP_ONE_DEGREE, val_empt);
-  button_control(val_button_010, -STEP_ONE_DEGREE*10, val_button_010_old);
-  button_control(val_button_01, -STEP_ONE_DEGREE, val_empt);
+  button_control(val_button_f90, STEP_ONE_DEGREE * 90, val_button_f90_old);
+  button_control(val_button_f10, STEP_ONE_DEGREE * 10, 9);
+  button_control(val_button_b90, -STEP_ONE_DEGREE * 90, val_button_b90_old);
+  button_control(val_button_b10, -STEP_ONE_DEGREE * 10, 9);
 
   delay(100);
 }

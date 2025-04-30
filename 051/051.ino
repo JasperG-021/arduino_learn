a3073aa1092498eb7acac9e68ae95c3f4f42d7c7
JasperG021 <kakuyasukuni@gmail.com>
2025-04-30 21:29:53 +0900

    update about convert to inch

--- 051/051.ino
+++ 051/051.ino
@@ -1,11 +1,8 @@
 #define PIN_TRIG 3
 #define PIN_ECHO 2
 
-#define sond_speed_cm 343 * 100
-#define sond_speed_inch 767 * 12
-float time_ping_s;
-float distance_cm;
-float distance_inch;
+#define sond_speed_cm_s 343 * 100
+#define sond_speed_inch_s 767 / 3600 * 5280 * 12
 
 void setup() {
   Serial.begin(115200);
@@ -15,7 +12,9 @@ void setup() {
 
 void loop() {
   int time_ping;
+  float time_ping_s;
   float distance_cm;
+  float distance_inch;
 
   digitalWrite(PIN_TRIG, LOW);
   delayMicroseconds(2);
@@ -24,11 +23,10 @@ void loop() {
   digitalWrite(PIN_TRIG, LOW);
 
   time_ping = pulseIn(PIN_ECHO, HIGH);
-
   time_ping_s = time_ping / 1000000.;
 
-  distance_cm = sond_speed_cm * time_ping_s / 2;
-  distance_inch = sond_speed_inch * time_ping_s / 2;
+  distance_cm = sond_speed_cm_s * time_ping_s / 2;
+  distance_inch = sond_speed_inch_s * time_ping_s / 2;
   Serial.println(String(distance_cm) + " cm");
   Serial.println(String(distance_inch) + " inch");
   Serial.println("----------");
